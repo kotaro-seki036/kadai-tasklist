ce3827cae7aff437c0fd6643f18401fe7ebb3672
kotaro.seki <KOUTAROU.SEKI@grp.tmnf.jp>
2025-06-04 12:01:41 +0900

    EditServlet.javaを作成

---
+++ src/main/java/controllers/EditServlet.java
@@ -0,0 +1,52 @@
+package controllers;
+
+import java.io.IOException;
+
+import jakarta.persistence.EntityManager;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import models.tasks;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class EditServlet
+ */
+@WebServlet("/edit")
+public class EditServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EditServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        // 該当のIDのタスク内容1件のみをデータベースから取得
+        tasks t = em.find(tasks.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        // タスク情報とセッションIDをリクエストスコープに登録
+        request.setAttribute("tasks", t);
+        request.setAttribute("_token", request.getSession().getId());
+
+        // メッセージIDをセッションスコープに登録
+        request.getSession().setAttribute("tasks_id", t.getId());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
+        rd.forward(request, response);
+    }
+}
